220222a5cbb20f8387bb5dff04ea69c60e42ce53
Taylor Martins <taylorfmartins@gmail.com>
2024-02-27 23:58:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é a pessoa mais importante pra mim! Te amo S2</h1>
+    return <h1>Você é a pessoa mais importante pra mim! Te amo 💖😍</h1>
 }
 
 export default Home;
