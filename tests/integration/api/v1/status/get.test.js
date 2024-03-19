36d645cc5a82a97f633c3ca9640aa1d5dedfc685
Taylor Martins <taylorfmartins@gmail.com>
2024-03-19 17:56:26 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should returns 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependecies.database.version).toEqual("16.2");
+  expect(responseBody.dependecies.database.max_connections).toEqual(100);
+  expect(responseBody.dependecies.database.open_connections).toEqual(1);
 });
